6a7f6326ec2417c6e2ec426fb136414ef4252e63
Söw Yet <multinaute@gmail.com>
2021-10-26 00:05:31 +0200

    add tailwind style

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,16 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 function about() {
   return (
     <div>
-      <div className="text-center">
-        <h1 className="px-6 mt-24 text-5xl font-extrabold tracking-tight text-green-500">
-          About Binaryville
-        </h1>
+      <div className="container mx-auto text-center">
+        <h1 className="">About Binaryville</h1>
         <StaticImage
           src="../images/powship.svg"
           alt="POW Ship"
           className="w-32 mx-auto"
         />
-        <p className="max-w-2xl px-6 mx-auto mt-4 text-xl text-gray-500">
+        <p className="px-6 mx-auto mt-4 text-xl text-gray-500">
           On the edge of the world stands the progressive town of Binaryville,
           where technology is a way of life.The robots who live, work, and play
           in Binaryville learn from an early age that magic is made simply by
@@ -21,6 +19,9 @@ function about() {
           thought and every sleeping dream is consumed with unique permutations…
           and endless possibilities.
         </p>
+        <div className="inline-block p-2 mt-2 mb-4 border-4 border-gray-500 rounded-md cursor-pointer active:bg-gray-500 hover:bg-gray-500 hover:text-white">
+          more info
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      md: "640px",
+    },
   },
   variants: {
     extend: {},
